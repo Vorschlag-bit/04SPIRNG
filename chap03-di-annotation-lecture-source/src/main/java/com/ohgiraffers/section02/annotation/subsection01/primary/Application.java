3f72f9ee09c1116e731eb1d085c82219ef4e886b
Vorschlag-bit <bustout@naver.com>
2024-08-09 10:51:57 +0900

    Primary 예제

--- chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/Application.java
+++ chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/Application.java
@@ -1,2 +1,20 @@
-package com.ohgiraffers.section02.annotation.subsection01.primary;public class Application {
+package com.ohgiraffers.section02.annotation.subsection01.primary;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.ohgiraffers.section02");
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+        PokemonService ps = context.getBean(PokemonService.class);
+        ps.pokemonAttack();
+    }
 }
