697da1fb32175819f39881cad4270971e3203099
Vorschlag-bit <bustout@naver.com>
2024-08-21 22:30:00 +0900

    commit

--- chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/common/Charmander.java
+++ chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/common/Charmander.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
+/* 설명. Pokemon 타입 중 CHarmander bean에 대한 우선권을 높이는 annotation */
 @Primary
 public class Charmander implements Pokemon{
     @Override
--- chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/PokemonService.java
+++ chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/PokemonService.java
@@ -9,7 +9,11 @@ public class PokemonService {
 
     private Pokemon pokemon;
 
-    @Autowired
+    // 이거 안 붙여도 생성자 주입 가능
+    /* 설명. 생성자 위에 @Autowired 생략이 가능하다(default 생성자 주입) (feat. 웬만하면 명시하자) */
+    // PokemonService의 기본 생성자가 없기 때문에 아래의 생성자가 기본으로 되어
+    // 초기화를 시도하고 그 파라미터로 Pokemon 객체가 요구되기 때문에 bean 속에서 알아서 찾아다준다.
+//    @Autowired
     public PokemonService(Pokemon pokemon) {
         this.pokemon = pokemon;
     }
