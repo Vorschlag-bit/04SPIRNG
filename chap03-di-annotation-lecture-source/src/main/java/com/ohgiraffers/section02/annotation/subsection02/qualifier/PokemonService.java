a46a0ea875efd0e788bd6b15beac5f47a32959ae
Vorschlag-bit <bustout@naver.com>
2024-08-26 23:42:15 +0900

    포켓몬 예제 끝

--- chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection02/qualifier/PokemonService.java
+++ chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection02/qualifier/PokemonService.java
@@ -1,2 +1,25 @@
-package com.ohgiraffers.section02.annotation.subsection02.qualifier;public class PokemonService {
+package com.ohgiraffers.section02.annotation.subsection02.qualifier;
+
+import com.ohgiraffers.section02.annotation.common.Pokemon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service("pokemonServiceQualifier")
+public class PokemonService {
+
+    /* 설명. 1. Qualifier를 활용한 field 주입 */
+//    @Autowired
+//    @Qualifier("squirtle")
+    private Pokemon pokemon;
+
+    /* 설명. 2. 생성자 주입 */
+    @Autowired
+    public PokemonService(@Qualifier("squirtle") Pokemon pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public void pokemonAttack() {
+        pokemon.attack();
+    }
 }
